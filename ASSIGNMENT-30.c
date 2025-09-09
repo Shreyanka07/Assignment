9b367f2cdcfd44a8cdadbeeaa455c594d8ac36cc
Shreyanka07 <maityshreyanka@gmail.com>
2025-09-09 22:41:21 +0530

    Add files via upload

---
+++ ASSIGNMENT-30.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int marks;
+	printf("your marks:");
+	scanf("%d",&marks);
+	if(marks>=90&&marks<=100){
+	printf("your grade is A+\n");
+	}
+	else if(marks>=80&&marks<=89)
+	{
+	printf("your grade is A\n");	
+	}
+	else if(marks>=70&&marks<=79)
+	{
+	printf("your grade is B+\n");
+    }
+	else if(marks>=60&&marks<=69)
+	{	
+	printf("your grade is B\n");
+	}
+	else if(marks>=50&&marks<=59)
+	{
+	printf("your grade is C+\n");
+    }
+	else if(marks>=40&&marks<=49)
+	{
+	printf("your grade is C\n");
+    }
+    else{
+    	printf("your are failed due to low marks");
+	}
+	return 0;
+}
