8d7863513ed9f97b0c41290b8a9ba7ff9bcf2854
Shreyanka07 <maityshreyanka@gmail.com>
2025-09-16 17:17:52 +0530

    Add files via upload

---
+++ ASSIGNMENT-34.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	int a=0,b=1;
+	int c;
+	int sum=0;
+	printf("Enter the number terms:");
+	scanf("%d",&n);
+	printf("Fibonacci Series:");
+	for(int i=1;i<=n;i++){
+		printf("%d",a);
+		sum+=a;
+		c=a+b;
+		a=b;
+		b=c;
+	}
+    printf("\nSum of the Fibonacci serise:%d\n",sum);
+	return 0;
+}
