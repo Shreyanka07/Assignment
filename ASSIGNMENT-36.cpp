d5d8d3b6d869aa1465b7e6fa862ce0f3da5c7e8b
Shreyanka07 <maityshreyanka@gmail.com>
2025-09-23 15:50:58 +0530

    Add files via upload

---
+++ ASSIGNMENT-36.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	char ch=A;
+	int r=4;
+	printf("Enter number of rows:");
+	scanf("%d",&r);
+	for(int i=1;i<=4;i++){
+	
+		for(int s=1;s<=r-i;s++){
+			printf("";)
+		}
+		for(int j=1;j<=i;j++){
+			printf("%c",ch);
+			ch++;
+		}
+		printf("\n")''
+	}
+     return 0;	 
+}
